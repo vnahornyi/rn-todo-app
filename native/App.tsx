04613c2049bc7b2fd743e560b7723e56ffb0e1c6
vnahornyi <vnahornyi@outlook.com>
2023-09-07 10:59:17 +0300

    added app provider for mobile

--- native/App.tsx
+++ native/App.tsx
@@ -38,6 +38,8 @@ import useLocale from "../shared/hooks/useLocale";
 import ThemeProvider from "./providers/ThemeProvider";
 import createStyles from "./utils/createStyles";
 import useWakeUp from "./hooks/useWakeUp";
+import AppProvider from "./providers/AppProvider";
+import useAppState from "./hooks/useAppState";
 
 export type RootScreensType = {
   FirstOnboarding: undefined;
@@ -59,9 +61,11 @@ const Tabs = createBottomTabNavigator<RootScreensType>();
 const App: React.FC = () => {
   return (
     <RootProvider>
-      <ThemeProvider>
-        <Navigator />
-      </ThemeProvider>
+      <AppProvider>
+        <ThemeProvider>
+          <Navigator />
+        </ThemeProvider>
+      </AppProvider>
     </RootProvider>
   );
 };
@@ -69,11 +73,17 @@ const App: React.FC = () => {
 const Navigator: React.FC = () => {
   const { isLight, preparePreference } = useTheme();
   const { prepareTodos } = useTodos();
+  const { prepareAppState, shouldSkipIntroScreen } = useAppState();
   const { prepareLocale } = useLocale();
 
   useEffect(() => {
     const load = async () => {
-      await Promise.all([prepareTodos(), preparePreference(), prepareLocale()]);
+      await Promise.all([
+        prepareTodos(),
+        preparePreference(),
+        prepareLocale(),
+        prepareAppState(),
+      ]);
       SplashScreen.hide();
     };
 
@@ -90,7 +100,9 @@ const Navigator: React.FC = () => {
       />
       <NavigationContainer theme={isLight ? lightTheme : darkTheme}>
         <Stack.Navigator
-          initialRouteName="FirstOnboarding"
+          initialRouteName={
+            shouldSkipIntroScreen ? "TabsRoot" : "FirstOnboarding"
+          }
           screenOptions={{
             headerShown: false,
             animation: "slide_from_right",
--- shared/constants/storageKeys.ts
+++ shared/constants/storageKeys.ts
@@ -1,2 +1,4 @@
 export const MOBILE_PREFERENCE = "mobile/preference";
+export const MOBILE_INTROSKIPPED = "mobile/intro_skipped";
+
 export const WEB_LANGUAGE = "web/language";
