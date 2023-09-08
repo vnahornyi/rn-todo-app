ee85a3b47c4985679d1f401c5b7d3faa06789f19
vnahornyi <vnahornyi@outlook.com>
2023-09-08 11:53:45 +0300

    added animations

--- native/components/CatCard.tsx
+++ native/components/CatCard.tsx
@@ -1,5 +1,5 @@
 import { memo, useMemo } from "react";
-import { Text, View } from "react-native";
+import { Text, ViewToken } from "react-native";
 import FastImage from "react-native-fast-image";
 
 import { pixelSizeVertical } from "../utils/normalize";
@@ -8,29 +8,44 @@ import TYPOGRAPHY from "../constants/typography";
 import SIZES from "../constants/sizes";
 import calculateCatCardHeight from "../utils/calculateCatCardHeight";
 import createStyles from "../utils/createStyles";
+import Animated, {
+  SharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
 
 type PropsType = CatType & {
   columns: number;
+  viewableItems: SharedValue<ViewToken[]>;
 };
 
 const CatCard: React.FC<PropsType> = ({
   reference_image_id,
   name,
-
+  viewableItems,
   columns,
+  id,
 }) => {
   const styles = useStyles();
+  const height = calculateCatCardHeight(columns);
 
-  const cardStyles = useMemo(
-    () => [
-      styles.card,
-      {
-        height: calculateCatCardHeight(columns),
-        width: SIZES.screenWidth / columns,
-      },
-    ],
-    [columns]
-  );
+  const cardStyles = useAnimatedStyle(() => {
+    const isVisible = !!viewableItems.value
+      .filter((item) => item.isViewable)
+      .find((viewableItem) => viewableItem.item.id === id);
+
+    return {
+      ...styles.card,
+      height,
+      width: SIZES.screenWidth / columns,
+      opacity: withTiming(isVisible ? 1 : 0),
+      transform: [
+        {
+          scale: withTiming(isVisible ? 1 : 0.6),
+        },
+      ],
+    };
+  }, [columns]);
 
   const nameStyles = useMemo(
     () => [styles.title, columns < 2 ? TYPOGRAPHY.title : TYPOGRAPHY.body],
@@ -38,7 +53,7 @@ const CatCard: React.FC<PropsType> = ({
   );
 
   return (
-    <View style={cardStyles}>
+    <Animated.View style={cardStyles}>
       <FastImage
         source={{
           uri: `https://cdn2.thecatapi.com/images/${reference_image_id}.jpg`,
@@ -48,7 +63,7 @@ const CatCard: React.FC<PropsType> = ({
         resizeMode={FastImage.resizeMode.cover}
       />
       <Text style={nameStyles}>{name}</Text>
-    </View>
+    </Animated.View>
   );
 };
 
